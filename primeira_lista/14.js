8f92aff8b22f1a61739ca28ac0ad5f18bdd5d009
Apolinario <whoami.apolo@gmail.com>
2024-08-31 21:16:25 -0300

    corrigindo exercicio

--- primeira_lista/14.js
+++ primeira_lista/14.js
@@ -5,14 +5,16 @@ utilizando um loop for ou while.
 const input = require("prompt-sync")();
 
 const fatorial = (element) => {
-  if (element === 0 || e === 1) {
+  if (element === 0 || element === 1) {
     return 1;
   }
 
-  while(e > 1) {
-    return element * fatorial(e - 1);
+  while(element > 1) {
+    return element * fatorial(element - 1);
   }
 };
 
 const inFatorial = parseInt(input("Digite um número: "));
 console.log(fatorial(inFatorial));
+
+console.log("Fim do programa");
